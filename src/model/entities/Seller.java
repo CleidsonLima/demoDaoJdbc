063656ab6e44af871fb16bc404a5aa7ed2cb2233
CleidsonLima <cleidson_lima@outlook.com>
2025-04-02 15:00:52 -0300

    Department entity class

---
+++ src/model/entities/Seller.java
@@ -0,0 +1,17 @@
+package model.entities;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Seller implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	private Integer id;
+	private String name;
+	private String email;
+	private Date birthDate;
+	private Double baseSalary;
+	
+	
+
+}
